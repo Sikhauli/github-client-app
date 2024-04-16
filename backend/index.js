0ed51910fbf36fdef78602c5e64a6454ee30182e
sikhauli <sikhaulivl@gmail.com>
2024-04-16 14:27:49 +0200

    update server

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const clientWebsite = "https://github-client-app-api.onrender.com/";
+const clientWebsite = "https://github-client-app.onrender.com";
 
 //connect to DB
 //listen after db connection has been est
