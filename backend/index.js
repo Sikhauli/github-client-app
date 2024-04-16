5fc0d0f957bffb3c1f19fec4d6a2e6cd5739c759
sikhauli <sikhaulivl@gmail.com>
2024-04-16 14:10:34 +0200

    update server

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,19 @@ const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 const mongoose = require("mongoose");
-
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
 
 // import routes
 const commitRoutes = require('./src/commits/route');
 const summaryRoutes = require('./src/summary/routes');
 const callbackRoutes = require('./src/callback/routes');
 
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const clientWebsite = "https://github-client-app-api.onrender.com/";
 
 //connect to DB
 //listen after db connection has been est
@@ -26,6 +27,13 @@ mongoose
         });
     })
     .catch((error) => console.log(error));
+
+app.use(
+    cors({
+        origin: clientWebsite,
+        credentials: true,
+    })
+);    
 
 app.use('/api/commits', commitRoutes);
 app.use('/api/repo',  summaryRoutes) 
